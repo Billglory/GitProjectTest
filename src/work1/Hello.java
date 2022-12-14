ff8d7529797a1b1bc5f7ec0243cabf9735a0f257
Bill Glory <billglory90@gmail.com>
2022-12-14 14:04:34 +0300

    started hello feature

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -4,5 +4,6 @@ package work1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println(" Work1 started ");
+        System.out.println("Developing Hello feature ");
     }
 }
