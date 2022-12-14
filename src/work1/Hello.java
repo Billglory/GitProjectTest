a006f5a2909b2f27a51a2df3c0c0941f9bfa18fa
Bill Glory <billglory90@gmail.com>
2022-12-14 14:32:50 +0300

    added more hello feature

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -5,5 +5,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println(" Work1 started ");
         System.out.println("Developing Hello feature ");
+        System.out.println(" Local Repo İs already publishe Githubd");
+        System.out.println("Doing more work");
     }
 }
